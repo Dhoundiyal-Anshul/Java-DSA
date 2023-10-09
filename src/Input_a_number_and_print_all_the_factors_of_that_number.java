3e15a05a01d1a3e89d0ab6cbcefd0f8d8804e1c8
Dhoundiyal-Anshul <dhoundiyalbest2010@gmail.com>
2023-10-09 17:47:20 +0530

    updated code on factors of a number

--- src/Input_a_number_and_print_all_the_factors_of_that_number.java
+++ src/Input_a_number_and_print_all_the_factors_of_that_number.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Input_a_number_and_print_all_the_factors_of_that_number {
     public static void main(String[] args) {
-        System.out.println("enter the number");
+        System.out.print("enter the number-");
+        Scanner input = new Scanner(System.in);
+        int num= input.nextInt();
+//                while(temp>0){
+//            if(num%temp==0){
+//                System.out.print(temp+" ");
+//            }
+//            temp-=1;
+        for(int temp=num;temp>0;temp-=1) {
+            if (num % temp == 0) {
+                System.out.print(temp + " ");
+            }
+        }
+
     }
 }
